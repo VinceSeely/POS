41105eb45aed2df6693ac7ae51ead4c37fc90b06
Adam E Nielsen <nielsenad@uwplatt.edu>
2017-12-06 10:48:52 -0600

    updated inventory for Emily

--- POSSolution/POSBackend/Inventory.cs
+++ POSSolution/POSBackend/Inventory.cs
@@ -11,9 +11,17 @@ namespace POSBackend
         private List<Item> inventory;
         private List<Coupon> couponList;
 
+        /**
+         * Searches for the item in the list 
+         */
+        private Item search(Item item)
+        {
+            return inventory.Find(item);
+        }
+
         public void addItem(Item item)
         {
-            inventory.
+            
         }
     }
 }
