25f78f1ad5abc69367e0194623a1910a814c040e
Adam E Nielsen <nielsenad@uwplatt.edu>
2017-12-06 10:28:11 -0600

    added backend project

---
+++ POSSolution/POSBackend/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSBackend
+{
+    class Inventory
+    {
+        private List<Item> inventory;
+        private List<Coupon> couponList;
+
+        public void addItem(Item item)
+        {
+            inventory.
+        }
+    }
+}
