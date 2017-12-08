0a338f30c5686b44c53aebeaf6218a80d6e68dc6
Timothy T Allen <allenti@uwplatt.edu>
2017-12-08 10:18:49 -0600

    Created Retailer, coupon and updated item class

--- POSSolution/POSBackend/Coupon.cs
+++ POSSolution/POSBackend/Coupon.cs
@@ -2,5 +2,15 @@
 {
     internal class Coupon
     {
+        private int sku;
+        private int itemId;
+        private int amountOff;
+
+        public Coupon(int sku, int itemId, int amountOff)
+        {
+            this.sku = sku;
+            this.itemId = itemId;
+            this.amountOff = amountOff;
+        }
     }
 }
