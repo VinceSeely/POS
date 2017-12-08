4577f4744c9b9794644d5c5d925d745ce97dfa36
Adam E Nielsen <nielsenad@uwplatt.edu>
2017-12-08 11:01:07 -0600

    Added CreditCard Class

--- POSSolution/POSBackend/Coupon.cs
+++ POSSolution/POSBackend/Coupon.cs
@@ -1,16 +1,10 @@
 ﻿namespace POSBackend
 {
+    /**
+     * Entity class for a coupon object
+     */
     internal class Coupon
     {
-        private int sku;
-        private int itemId;
-        private int amountOff;
 
-        public Coupon(int sku, int itemId, int amountOff)
-        {
-            this.sku = sku;
-            this.itemId = itemId;
-            this.amountOff = amountOff;
-        }
     }
 }
---
+++ POSSolution/POSBackend/CreditCard.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSBackend
+{
+    class CreditCard
+    {
+        private String CardNumber;
+        private enum CardTypes {EXPRESS=3,VISA=4,MASTER=5,DISCOVER=6};
+        private CardTypes CardType;
+        private int CVV;
+
+        private void getCardType()
+        {
+            int firstNum = Convert.ToInt32(CardNumber[0]);
+            switch (firstNum)
+            {
+                case (int)CardTypes.EXPRESS:
+                    CardType = CardTypes.EXPRESS;
+                    break;
+
+                case (int)CardTypes.VISA:
+                    CardType = CardTypes.VISA;
+                    break;
+
+                case (int)CardTypes.MASTER:
+                    CardType = CardTypes.MASTER;
+                    break;
+                case (int)CardTypes.DISCOVER:
+                    CardType = CardTypes.DISCOVER;
+                    break;
+            }
+        }
+    }
+}
