bdd0bddb5bcedaa529051071546ad98242b222fa
Adam E Nielsen <nielsenad@uwplatt.edu>
2017-12-08 16:14:05 -0600

    Forgot to save everything

--- POSSolution/POSBackend/ItemQuantityPair.cs
+++ POSSolution/POSBackend/ItemQuantityPair.cs
@@ -20,5 +20,13 @@ namespace POSBackend
         public Item item { get; set; }
 
         public int quantity { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var realObject = obj as ItemQuantityPair;
+            if (realObject == null)
+                return false;
+            return item.Equals(realObject.item);
+        }
     }
 }
